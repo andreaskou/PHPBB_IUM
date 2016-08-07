14f0901848cca85db902e30ecc64eddd55ff7df9
andreaskou <akourtidis@gmail.com>
2016-08-07 19:27:38 +0200

    create table ium on install, on uninstall drop table

--- migrations/add_module.php
+++ migrations/add_module.php
@@ -47,4 +47,34 @@ class add_module extends migration
     			)),
     		);
     }
+
+    public function update_schema()
+    {
+
+        return array(
+            'add_tables'   => array(
+                $this->table_prefix . 'ium'   => array(
+                    'COLUMNS'   => array(
+                        'reminder_id'               => array('UINT', null, 'auto_increment'),
+                        'user_id'                   => array('UINT', 0),
+                        'username'                  => array('VCHAR', ''),
+                        'remind_counter'            => array('UINT', '0'),
+                        'previous_sent_date'        => array('TIMESTAMP', 0),
+                        'reminder_sent_date'        => array('TIMESTAMP', 0),
+                        'ignore'                    => array('UINT', 0)
+                        ),
+                    'PRIMARY_KEY'   => 'reminder_id',
+                ),
+            ),
+        );
+    }
+
+    public function revert_schema()
+    {
+        return array(
+            'drop_table'    =>  array(
+                $this->table_prefix . 'ium',
+            ),
+        );
+    }
 }
