6c6f6aa8084b0e57d35e43e0b24941e2355db36f
andreaskou <akourtidis@gmail.com>
2016-08-09 20:48:15 +0200

    Add more configuration parameters

--- migrations/add_module.php
+++ migrations/add_module.php
@@ -34,11 +34,11 @@ class add_module extends migration
     	return array(
     		array('config.add',	array('andreask_ium_enable', 0)),
             array('config.add', array('andreask_ium_interval', 30)),
-            array('config.add', array('andreask_ium_top_user_treads', 0)),
-            array('config.add', array('andreask_ium_top_user_treads_count', 5)),
-            array('config.add', array('andreask_ium_top_treads', 0)),
-            array('config.add', array('andreask_ium_top_treads_count', 5)),
-            array('config.add', array('andreask_ium_self_delete', 5)),
+            array('config.add', array('andreask_ium_top_user_threads', 0)),
+            array('config.add', array('andreask_ium_top_user_threads_count', 5)),
+            array('config.add', array('andreask_ium_top_forum_threads', 0)),
+            array('config.add', array('andreask_ium_top_forum_threads_count', 5)),
+            array('config.add', array('andreask_ium_self_delete', 0)),
 
             array('module.add',	array(
                 'acp',
