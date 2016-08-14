489c4c79420298211a55c96e69cff979c64f0e47
andreaskou <akourtidis@gmail.com>
2016-08-14 22:53:43 +0200

    add container and set send() for cron

--- cron/send_reminder.php
+++ cron/send_reminder.php
@@ -4,11 +4,13 @@
 namespace andreask\ium\cron;
 
 use andreask\ium\classes\reminder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class send_reminder extends \phpbb\cron\task\base
 {
     protected $config;
     protected $user;
+	protected $container;
     protected $log;
 
     /**
@@ -17,10 +19,11 @@ class send_reminder extends \phpbb\cron\task\base
      * @param \phpbb\config\config $config The config
      */
 
-    public function __construct(\phpbb\config\config $config,\phpbb\log\log $log,\phpbb\user $user)
+    public function __construct(\phpbb\config\config $config,\phpbb\log\log $log,\phpbb\user $user, ContainerInterface $container)
     {
         $this->config = $config;
         $this->user = $user;
+        $this->container = $container;
         $this->log = $log;
 
     }
@@ -32,15 +35,12 @@ class send_reminder extends \phpbb\cron\task\base
      */
     public function run()
     {
-        // Your code goes here.
-        // Do the actions that are the purpose for the Cron Job that we have created.
-
-
-		// Do not forget to update the configuration variable for last run time.
+        // Do not forget to update the configuration variable for last run time.
+		$reminder = $this->container->get('andreask.ium.cron.send_reminder');
+		$reminder->send($this->config['andreask_ium_email_limit']);
 		$this->config->set('test_task_last_gc', time());
-
 		// for now add a line to logs.
-		$this->log->add('admin', 54 , '127.0.0.1', 'Sent reminders', time());
+		$this->log->add('send_reminder', 54 , '127.0.0.1', 'Sent reminders', time());
     }
 
     /**
