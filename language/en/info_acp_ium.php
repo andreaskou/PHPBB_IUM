d6eb6037664e28badfe0e0d4c91e113792b10e6e
andreaskou <akourtidis@gmail.com>
2016-08-14 22:50:24 +0200

    Add lang

--- language/en/info_acp_ium.php
+++ language/en/info_acp_ium.php
@@ -28,6 +28,7 @@ $lang = array_merge($lang, array(
                                         3   =>  'Admin deactivated',
                                         4   =>  'Permanently Banned',
                                         5   =>  'Temporarily Banned'],
+		'NEVER_CONNECTED'		=> 'User never connected',
 
     // Inactive users list
     'ACP_IUM_NODATE'        =>  'User is <strong>not</strong> disabled',
@@ -39,21 +40,30 @@ $lang = array_merge($lang, array(
     'REMINDER_DATE'         =>  'Last Reminder Sent',
     'NO_REMINDER_SENT_YET'  =>  'Not sent any reminders yet',
 
-    // Configuration APC
-        'ANDREASK_IUM_ENABLE'       =>  'Enable Advanced Inactive User Manager?',
-        'ANDREASK_IUM_INTERVAL'             =>  'Interval days back',
-        'ANDREASK_IUM_TOP_USER_THREADS'             =>  'Include user top threads?',
-        'ANDREASK_IUM_TOP_USER_THREADS_COUNT'       =>  'How many threads?',
-        'ANDREASK_IUM_TOP_FORUM_THREADS'          =>  'Include forum top threads?',
-        'ANDREASK_IUM_TOP_FORUM_THREADS_COUNT'    =>  'How many threads?',
-        'ANDREASK_IUM_SELF_DELETE'  => 'User self delete',
+    // ACP Settings
+	'ANDREASK_IUM_ENABLE'       				=>  'Enable Advanced Inactive User Manager?',
+	'ANDREASK_IUM_INTERVAL'             		=>  'Interval',
+	'ANDREASK_IUM_EMAIL_LIMIT'					=>	'Limit E-mails',
+	'ANDREASK_IUM_TOP_USER_THREADS'             =>  'Include user top threads?',
+	'ANDREASK_IUM_TOP_USER_THREADS_COUNT'       =>  'How many threads?',
+	'ANDREASK_IUM_TOP_FORUM_THREADS'          	=>  'Include forum top threads?',
+	'ANDREASK_IUM_TOP_FORUM_THREADS_COUNT'    	=>  'How many threads?',
+	'ANDREASK_IUM_SELF_DELETE'  				=> 	'User self delete',
 
-        'ANDREASK_IUM_ENABLE_EXPLAIN'                   =>  'If enabled, the extension will start sending reminders to "sleepers". FOR NOW NON OF THESE CONFIGURATIONS ARE ACTIVE/WORKING <strong>EXT UNDER DEVELOPMENT</strong>',
-        'ANDREASK_IUM_INTERVAL_EXPLAIN'                 =>  'It is the interval of days back to count to consider a user a "sleeper". Default is 30 days',
-        'ANDREASK_IUM_TOP_USER_THREADS_EXPLAIN'         =>  'If enabled, mail will include users top active threads since users last visit.',
-        'ANDREASK_IUM_TOP_USER_THREADS_COUNT_EXPLAIN'   =>  'How many user top threads should be included in mail?',
-        'ANDREASK_IUM_TOP_FORUM_THREADS_EXPLAIN'        =>  'If enabled, mail will include forum top threads since users last visit?',
-        'ANDREASK_IUM_TOP_FORUM_THREADS_COUNT_EXPLAIN'  =>  'How many forum threads should be included in mail?',
-        'ANDREASK_IUM_SELF_DELETE_EXPLAIN'              =>  'If enabled, the user will be able to remove himself from the board.',
+	// ACP Explanations
+	'ANDREASK_IUM_ENABLE_EXPLAIN'                   =>  'If enabled, the extension will start sending reminders to "sleepers". FOR NOW NON OF THESE CONFIGURATIONS ARE ACTIVE/WORKING <strong>EXT UNDER DEVELOPMENT</strong>',
+	'ANDREASK_IUM_INTERVAL_EXPLAIN'                 =>  'It is the interval of days back to count to consider a user a "sleeper". Recommended is 30 days',
+	'ANDREASK_IUM_EMAIL_LIMIT_EXPLAIN'				=>	'Amount of the reminders that can be sent per day. Recommended is ~250. But do check with your host for any limit of e-mails per day',
+	'ANDREASK_IUM_TOP_USER_THREADS_EXPLAIN'         =>  'If enabled, mail will include users top active threads since users last visit.',
+	'ANDREASK_IUM_TOP_USER_THREADS_COUNT_EXPLAIN'   =>  'Amount of user\'s top threads that should be included in e-mail',
+	'ANDREASK_IUM_TOP_FORUM_THREADS_EXPLAIN'        =>  'If enabled, mail will include forum top threads since users last visit',
+	'ANDREASK_IUM_TOP_FORUM_THREADS_COUNT_EXPLAIN'  =>  'Amount of forum threads that should be included in e-mail',
+	'ANDREASK_IUM_SELF_DELETE_EXPLAIN'              =>  'If enabled, the user will be able to remove himself from the board.',
+
+	// Legend
+	'ACP_IUM_SETTINGS'              =>  'Inactive User Manager Settings',
+	'ACP_IUM_MAIL_SETTINGS'         =>  'Reminder Settings',
+	'ACP_IUM_MAIL_INCLUDE_SETTINGS'	=>  'Reminder Include Settings',
+	'ACP_IUM_DANGER'				=>  'Danger Area',
 
 ));
