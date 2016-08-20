45f283e4be9f3cc8db5fb55b00b2cbab30c6e8b1
andreaskou <akourtidis@gmail.com>
2016-08-21 01:53:43 +0200

    add containerInterface, remove log for debug, add load lang file for log, fix query (ignore users)

--- cron/send_reminder.php
+++ cron/send_reminder.php
@@ -10,7 +10,6 @@ class send_reminder extends \phpbb\cron\task\base
     protected $config;
     protected $user;
 	protected $container;
-    protected $log;
 
     /**
      * Constructor.
@@ -18,12 +17,11 @@ class send_reminder extends \phpbb\cron\task\base
      * @param \phpbb\config\config $config The config
      */
 
-    public function __construct(\phpbb\config\config $config, \phpbb\log\log $log, \phpbb\user $user, ContainerInterface $container)
+    public function __construct(\phpbb\config\config $config, \phpbb\user $user, ContainerInterface $container)
     {
         $this->config = $config;
         $this->user = $user;
         $this->container = $container;
-        $this->log = $log;
     }
 
     /**
@@ -33,15 +31,10 @@ class send_reminder extends \phpbb\cron\task\base
      */
     public function run()
     {
-        // Do not forget to update the configuration variable for last run time.
-		$this->log->add('admin', 54 , '127.0.0.1', 'In the cron run before sending reminders', time());
-
 		$reminder = $this->container->get('andreask.ium.classes.reminder');
 		$reminder->send((int) $this->config['andreask_ium_email_limit']);
-
+		// Update last run datetime stamp
 		$this->config->set('send_reminder_last_gc', time());
-		// for now add a line to logs.
-		$this->log->add('admin', 54 , '127.0.0.1', 'Sent reminders', time());
     }
 
     /**
@@ -53,19 +46,15 @@ class send_reminder extends \phpbb\cron\task\base
 
     public function should_run()
     {
-    	$run = (bool) ($this->config['send_reminder_last_gc'] < (time() - $this->config['send_reminder_gc']));
-		$this->log->add('admin','54','127.0.0.1',"Do I need to run? (".$run. ") last_gc ".date("Y-m-d H:i:s",$this->config['send_reminder_last_gc'])." now :" .date("Y-m-d H:i:s",time())." - ". $this->config['send_reminder_gc'] ." Seconds", time());
-        return (bool) $this->config['send_reminder_last_gc'] < strtotime('10 minutes ago');
+		return $this->config['send_reminder_last_gc'] < (time() - $this->config['send_reminder_gc']);
     }
 
 	/**
-	 * @return bool enable cron task if
+	 * @return bool enable cron task if it's enabled in the ext.
 	 */
 
 	public function is_runnable()
     {
-    	$test = $this->config['andreask_ium_enable'];
-    	$this->log->add('admin','54','127.0.0.1',"Can I run? : $test ", time());
         return (bool) $this->config['andreask_ium_enable'];
     }
 }
