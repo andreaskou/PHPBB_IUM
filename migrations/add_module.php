e658f2fb96f0f03b5faa07ac0057a755a0282480
andreaskou <akourtidis@gmail.com>
2016-08-29 15:39:30 +0200

    travis build fix

--- migrations/add_module.php
+++ migrations/add_module.php
@@ -17,7 +17,7 @@ use phpbb\db\migration\migration;
 
 class add_module extends migration
 {
-    private $schema_name = 'ium_reminder';
+    private $schema_name='ium_reminder';
 
 //	public function effectively_installed()
 //	{
