150a4bf72dc5f24fd040804ce2a97d0365d9a2bc
andreaskou <akourtidis@gmail.com>
2016-08-29 13:10:54 +0200

    lang for e-mail body

---
+++ language/en/body.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * @package phpBB Extension - Advanced Inactive User Manager
+ * @copyright (c) 2013 phpBB Group
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'INCLUDE_USER_TOPICS'				=>	'Here are some links to topics that you\'ve been active.'
+));
