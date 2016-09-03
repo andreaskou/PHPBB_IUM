75a6269b1f7746c7015d98f82216379ac61c6a69
andreaskou <akourtidis@gmail.com>
2016-09-03 19:34:22 +0200

    new function get_forum_posts gets (guess what)

--- classes/top_topics.php
+++ classes/top_topics.php
@@ -55,6 +55,7 @@ class top_topics {
 			GROUP BY forum_id, topic_id 
 			ORDER BY posts_count DESC';
 
+			// limit results to configuration
 			$result = $this->db->sql_query_limit($sql, $this->config['andreask_ium_top_user_threads_count']);
 			$active_t_row = array();
 
@@ -92,11 +93,72 @@ class top_topics {
 		}
 	}
 
+
+	public function get_forum_top_topics($id)
+	{
+		if (!$id)
+		{
+			return false;
+		}
+		else if ($this->config['andreask_ium_top_forum_threads'] == 0)
+		{
+			return null;
+		}
+		if (!$this->user_id)
+		{
+			$this->set_id($id);
+		}
+
+		// Obtain most active topic of board
+		$sql = 'SELECT forum_id, topic_id, count(post_id) as posts_count
+		FROM ' . POSTS_TABLE . ' 
+		WHERE post_postcount = 1 
+		GROUP BY forum_id, topic_id 
+		ORDER BY posts_count DESC';
+
+		// limit results to configuration
+		$result = $this->db->sql_query_limit($sql, $this->config['andreask_ium_top_forum_threads_count']);
+		$active_t_row = array();
+
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$active_t_row[] = $row;
+		};
+
+		$this->db->sql_freeresult($result);
+
+		if ( !empty($active_t_row) )
+		{
+			foreach ($active_t_row as $key => &$topic)
+			{
+				if ( !$this->user_access($topic['forum_id']) )
+				{
+					// delete if user does not have access to the topic any more, I just couldn't find a better place to do this.
+					unset($active_t_row[$key]);
+				}
+				else
+				{
+					// else complete the puzzle.
+					$sql = 'SELECT topic_title as title
+						FROM ' . TOPICS_TABLE . '
+						WHERE topic_id = ' . $topic['topic_id'];
+
+					$result = $this->db->sql_query($sql);
+					$topic['topic_title'] = (string) $this->db->sql_fetchfield('title');
+					$this->db->sql_freeresult($result);
+				}
+			}
+			return $active_t_row;
+		}
+		return null;
+	}
+
 	/**
 	 * Give the amount of post for user
 	 * @param $user user_id
 	 * @return bool|int Number of posts or else return false
 	 */
+
 	private function user_post_count($user_id)
 	{
 		if ($user_id)
