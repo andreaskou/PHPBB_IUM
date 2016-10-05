3b92890f5207b03ac44f8c2bac925173a1775ccf
andreaskou <akourtidis@gmail.com>
2016-10-05 12:35:33 +0200

    spacing

--- cron/send_reminder.php
+++ cron/send_reminder.php
@@ -7,54 +7,57 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class send_reminder extends \phpbb\cron\task\base
 {
-	protected $config;
-	protected $user;
-	protected $container;
+    protected $config;
+    protected $user;
+    protected $container;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param \phpbb\config\config $config The config
-	 */
+    /**
+    * Constructor.
+    *
+    * @param \phpbb\config\config $config The config
+    */
 
-	public function __construct(\phpbb\config\config $config, \phpbb\user $user, ContainerInterface $container)
-	{
-		$this->config = $config;
-		$this->user = $user;
-		$this->container = $container;
-	}
+    public function __construct(\phpbb\config\config $config, \phpbb\user $user, ContainerInterface $container)
+    {
+        $this->config = $config;
+        $this->user = $user;
+        $this->container = $container;
+    }
 
-	/**
-	 * Runs this cron task.
-	 *
-	 * @return null
-	 */
-	public function run()
-	{
-		$reminder = $this->container->get('andreask.ium.classes.reminder');
-		$reminder->send((int) $this->config['andreask_ium_email_limit']);
-		// Update last run datetime stamp
-		$this->config->set('send_reminder_last_gc', time());
-	}
+    /**
+    * Runs this cron task.
+    *
+    * @return null
+    */
 
-	/**
-	 * Returns whether this cron task should run now, because enough time
-	 * has passed since it was last run.
-	 *
-	 * @return bool
-	 */
+    public function run()
+    {
+        $reminder = $this->container->get('andreask.ium.classes.reminder');
+        $reminder->send((int) $this->config['andreask_ium_email_limit']);
+        // Update last run datetime stamp
+        $this->config->set('send_reminder_last_gc', time());
+    }
 
-	public function should_run()
-	{
-		return $this->config['send_reminder_last_gc'] < (time() - $this->config['send_reminder_gc']);
-	}
 
-	/**
-	 * @return bool enable cron task if it's enabled in the ext.
-	 */
+    /**
+    * Returns whether this cron task should run now, because enough time
+    * has passed since it was last run.
+    *
+    * @return bool
+    */
 
-	public function is_runnable()
-	{
-		return (bool) $this->config['andreask_ium_enable'];
-	}
+    public function should_run()
+    {
+        return $this->config['send_reminder_last_gc'] < (time() - $this->config['send_reminder_gc']);
+    }
+
+    /**
+    * @return bool enable cron task if it's enabled in the ext.
+    */
+
+    public function is_runnable()
+    {
+        return (bool) $this->config['andreask_ium_enable'];
+    }
 }
+
