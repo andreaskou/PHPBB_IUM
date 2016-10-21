8d2f0b6e2e6f38b09573001b4ec9a921a97d3a50
andreaskou <akourtidis@gmail.com>
2016-10-22 00:07:34 +0200

    merge from testing, looks to work

--- classes/reminder.php
+++ classes/reminder.php
@@ -134,10 +134,7 @@ class reminder
 				}
 
 				$messenger = new \messenger(false);
-
-				// TODO check if this is OK to do...
-				// $this->request->server('SERVER_ADDR');
-				// mail andtiabuse headers
+				// mail headers
 				$messenger->headers('X-AntiAbuse: Board servername - ' . $this->config['server_name']);
 				$messenger->headers('X-AntiAbuse: Username - ' . $this->user->data['username']);
 				$messenger->headers('X-AntiAbuse: User_id - ' . $this->user->data['user_id']);
@@ -414,7 +411,7 @@ class reminder
 			$topic_links = null;
 
 			// If there are topics then prepare them for the e-mail.
-			if ( $top_user_topics = $topics->get_user_top_topics( $sleeper['user_id'] ) )
+			if ($top_user_topics = $topics->get_user_top_topics( $sleeper['user_id'], $sleeper['user_lastvisit'] ))
 			{
 				$topic_links = $this->make_topics($top_user_topics);
 			}
@@ -423,7 +420,7 @@ class reminder
 			$forum_links = null;
 
 			// If there are topics then prepare the for the mail.
-			if ( $top_forum_topics = $topics->get_forum_top_topics( $sleeper['user_id'] ) )
+			if ( $top_forum_topics = $topics->get_forum_top_topics( $sleeper['user_id'], $sleeper['user_lastvisit'] ))
 			{
 				$forum_links = $this->make_topics($top_forum_topics);
 			}
@@ -534,9 +531,9 @@ class reminder
 		foreach ($topics as $item)
 		{
 			$topic_links .= PHP_EOL;
-			$topic_links .= '"' . $item['topic_title'] . '"' . PHP_EOL;
-			$topic_links .= generate_board_url() . "/viewtopic." . $this->php_ext . "?f=" . $item['forum_id'] . "?&t=" . $item['topic_id'] . PHP_EOL;
 			$topic_links .= PHP_EOL;
+			$topic_links .= '"' . $item['topic_title'] . '"' . PHP_EOL;
+			$topic_links .= generate_board_url() . "/viewtopic." . $this->php_ext . "?f=" . $item['forum_id'] . "?&t=" . $item['topic_id'];
 		}
 
 		return $topic_links;
