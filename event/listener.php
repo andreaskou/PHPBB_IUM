60f3cc52a1d43372eea1ccbfa5b63f86b51ed8d5
andreaskou <akourtidis@gmail.com>
2016-10-24 17:16:19 +0200

    Bug fix, new users now have to be added to ium_table via the listener

--- event/listener.php
+++ event/listener.php
@@ -34,29 +34,39 @@ class listener implements EventSubscriberInterface
 	{
 		return array(
 			'core.login_box_redirect'	=>	'update_table_ium_reminder_counter',
-			'core.delete_user_after'	=>	'update_table_ium_reminder_user_deleted'
+			'core.delete_user_after'	=>	'update_table_ium_reminder_user_deleted',
+			'core.user_add_after'		=>	'update_table_ium_new_user',
 		);
 	}
 
+	// Reset reminder counter if users has returned!
 	public function update_table_ium_reminder_counter($event)
 	{
-		// Reset reminder counter if users has returned!
 		$update = $this->container->get('andreask.ium.classes.reminder');
 		$update->reset_counter($this->user->data['user_id']);
 	}
 
+	// Remove user from ium table if user is deleted (also from ext)
 	public function update_table_ium_reminder_user_deleted($event)
 	{
-
-		$mode = $event['mode'];
-		$action = $event['retain_username'];
 		$id = $event['user_ids'];
 
 		$delete = $this->container->get('andreask.ium.classes.delete_user');
 		$delete->clean_ium_table($id);
 
+		// Example for debugging...
 		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'deletion mode ' . $mode, time());
 		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'deletion action ' . $action, time());
 		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'deleted user_id ' . $id[0], time());
+	}
+	// If a user has registerd succesfuly add hit to the table.
+	// FIXME (I don't like this, need to simplify...)
+	public function update_table_ium_new_user($event)
+	{
+		$user_id = $event['user_id'];
+		$add = $this->container->get('andreask.ium.classes.reminder');
+		$add->new_user($user_id);
+		
+		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'user_id ' . $user_id, time());
 	}
 }
