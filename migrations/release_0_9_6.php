e4fe46c1dc8ca2b8b3d059243e29a03e2ef60020
andreaskou <akourtidis@gmail.com>
2016-10-31 22:13:32 +0100

    add config_text for exclude forums function, new release ver 0.9.6

---
+++ migrations/release_0_9_6.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+* This file is part of the phpBB Forum extension package
+* IUM (Inactive User Manager).
+*
+* @copyright (c) 2016 by Andreas Kourtidis
+* @license   GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the CREDITS.txt file.
+*/
+
+namespace andreask\ium\migrations;
+
+use phpbb\db\migration\migration;
+
+class release_0_9_6 extends migration
+{
+
+	private $schema_name='ium_reminder';
+
+	public function effectively_installed()
+	{
+		// return $this->db_tools->sql_column_exists($this->table_prefix . $this->schema_name, 'random');
+	}
+
+	static public function depends_on()
+	{
+		return array('\andreask\ium\migrations\release_0_9_1');
+	}
+
+	public function update_data()
+	{
+		return array(
+				array('config.add', array('andreask_ium_version',				'0.9.6')),
+				array('config_text.add', array('andreask_ium_ignore_forum',	'')),
+		);
+	}
+}
