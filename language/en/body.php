28085bfbf9427d6b3796983e656a90f2e4591eb5
andreaskou <akourtidis@gmail.com>
2016-11-02 02:00:54 +0100

    Fixed spelling, thanks to Cooper_si :)

--- language/en/body.php
+++ language/en/body.php
@@ -22,7 +22,7 @@ if ( empty($lang) || !is_array($lang) )
 }
 
 $lang = array_merge($lang, array(
-	'INCLUDE_USER_TOPICS'				=>	'Bellow some links to topics that you\'ve been active. %s',
-	'INCLUDE_FORUM_TOPICS'				=>	'Bellow some links to the most active topics of the board. %s',
+	'INCLUDE_USER_TOPICS'				=>	'Below some links to topics that you\'ve been active. %s',
+	'INCLUDE_FORUM_TOPICS'				=>	'Below some links to the most active topics of the board. %s',
 	'FOLOW_TO_DELETE'					=>	'Click on the following link to delete your account. %s',
 ));
