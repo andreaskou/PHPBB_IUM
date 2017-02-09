8a6454d8125bf649908732c44991dee42832e499
andreaskou <akourtidis@gmail.com>
2017-02-09 18:33:32 +0100

    future adding and comments

--- event/listener.php
+++ event/listener.php
@@ -20,33 +20,49 @@ class listener implements EventSubscriberInterface
 {
 
 	protected $log;
+	protected $config;
 	protected $user;
 	protected $container;
 
-	public function __construct(ContainerInterface $container, \phpbb\user $user, \phpbb\log\log $log)
+	public function __construct(ContainerInterface $container, \phpbb\user $user, \phpbb\config\config $config, \phpbb\log\log $log)
 	{
 		$this->container 	=	$container;
 		$this->user 		=	$user;
+		$this->config 		=	$config;
 		$this->log			=	$log;
 	}
 
+	/**
+	 * Subscribe to phpbb events and execute the appropriate scripts.
+	 * @return array List of events to subsribe with their represented methods.
+	 */
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.login_box_redirect'	=>	'update_table_ium_reminder_counter',
-			'core.delete_user_after'	=>	'update_table_ium_reminder_user_deleted',
-			'core.user_add_after'		=>	'update_table_ium_new_user',
+			'core.login_box_redirect'				=>	'update_table_ium_reminder_counter',
+			'core.delete_user_after'				=>	'update_table_ium_reminder_user_deleted',
+			'core.user_add_after'					=>	'update_table_ium_new_user',
+			// 'core.acp_users_display_overview'		=>	'add_remind_user_option',
+			// 'core.acp_users_overview_run_quicktool'	=>	'remind_single_user',
 		);
 	}
 
-	// Reset reminder counter if users has returned!
+	/**
+	 * Reset reminder counter if users has returned!
+	 * @param  array $event user_id of user to reset the counter for.
+	 * @return void
+	 */
 	public function update_table_ium_reminder_counter($event)
 	{
 		$update = $this->container->get('andreask.ium.classes.reminder');
 		$update->reset_counter($this->user->data['user_id']);
 	}
 
-	// Remove user from ium table if user is deleted (also from ext)
+	/**
+	 * Remove user from ium table when a user is deleted (by admin or by the ext)
+	 * @param  array $event user_id(s)
+	 * @return void
+	 */
 	public function update_table_ium_reminder_user_deleted($event)
 	{
 		$id = $event['user_ids'];
@@ -59,11 +75,45 @@ class listener implements EventSubscriberInterface
 		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'deletion action ' . $action, time());
 		// $this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'deleted user_id ' . $id[0], time());
 	}
-	// If a user has registerd succesfuly add him to the table.
+
+	/**
+	 * If a user has registerd succesfuly add him to the table.
+	 * @param  array $event user_id of the user to update in the table.
+	 * @return void
+	 */
 	public function update_table_ium_new_user($event)
 	{
 		$user_id = $event['user_id'];
 		$add = $this->container->get('andreask.ium.classes.reminder');
 		$add->new_user($user_id);
+	}
+
+	/**
+	 * Appends new option in quick tools on ACP user overview page.
+	 * @param array $event 'option' => 'langugage'.
+	 * @return void
+	 */
+	public function add_remind_user_option($event)
+	{
+		if ($this->config['andreask_ium_enable'])
+		{
+			$option = $event['quick_tool_ary'];
+			$option['andreask_ium_remind'] = 'ANDREASK_IUM_USERS_OVERVIEW_OPTION';
+			$event['quick_tool_ary'] = $option;
+		}
+	}
+
+	public function remind_single_user($event)
+	{
+		$action = $event['action'];
+		$user[] = $event['user_row'];
+
+		if ($action == 'andreask_ium_remind')
+		{
+			// $remind = $this->container->get('andreask.ium.classes.reminder');
+			// $remind->set_user($user);
+			// $remind->send('1');
+			// $remind->user($user['user_id']);
+		}
 	}
 }
