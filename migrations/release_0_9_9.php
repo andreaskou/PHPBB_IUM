c2793df00a97b5e7c32d9489812756f5a1b906b9
andreaskou <akourtidis@gmail.com>
2017-03-09 23:55:06 +0100

    new migrations adds config_text andreask_ium_ignored_groups for group ignore function

---
+++ migrations/release_0_9_9.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+* This file is part of the phpBB Forum extension package
+* IUM (Inactive User Manager).
+*
+* @copyright (c) 2016 by Andreas Kourtidis
+* @license   GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the CREDITS.txt file.
+*/
+
+namespace andreask\ium\migrations;
+
+use phpbb\db\migration\migration;
+
+class release_0_9_9 extends migration
+{
+
+	private $schema_name='ium_reminder';
+
+	public function effectively_installed()
+	{
+		return phpbb_version_compare($this->config['andreask_ium_version'], '0.9.9', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\andreask\ium\migrations\release_0_9_6');
+	}
+
+	public function update_data()
+	{
+		$group_ids = array();
+		return array(
+				array('config.update', array('andreask_ium_version',				'0.9.9')),
+				array('config_text.add', array('andreask_ium_ignored_groups', '')),
+		);
+	}
+}
