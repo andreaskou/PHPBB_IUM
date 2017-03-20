2a2ee561f19dab2d07bac346bdae7cb4c3fddfd3
andreaskou <akourtidis@gmail.com>
2017-03-21 00:09:36 +0100

    Use language helper

--- controller/main.php
+++ controller/main.php
@@ -32,6 +32,7 @@ class main
 	protected $container;
 	protected $table_name;
 	protected $u_action;
+	protected $language;
 
 	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\user $user, \phpbb\request\request $request, \phpbb\controller\helper $helper, \phpbb\template\template $template, ContainerInterface $container, $ium_table)
 	{
@@ -44,26 +45,28 @@ class main
 		$this->container    =   $container;
 		$this->table_name   =   $ium_table;
 		$this->u_action     =   append_sid(generate_board_url() . '/' . $this->user->page['page']);
+		$this->language		=	$this->container->get('andreask.ium.classes.language_helper');
 	}
 
 	public function handle($random)
 	{
-		$this->user->add_lang_ext('andreask/ium', 'user_self_delete_page');
+		$this->language->add_lang('andreask/ium', 'user_self_delete_page');
+		// $this->user->add_lang_ext('andreask/ium', 'user_self_delete_page');
 
 		if ($this->config['andreask_ium_enable'] == 0 || $this->config['andreask_ium_self_delete'] == 0)
 		{
-			return $this->helper->error($this->user->lang('PAGE_NOT_EXIST'), 404);
+			return $this->helper->error($this->language->lang('PAGE_NOT_EXIST'), 404);
 		}
 
 		// Check to see if user is logged in.
 		if ($this->user->data['user_id'] == ANONYMOUS)
 		{
-			return $this->helper->error(login_box('', $this->user->lang('HAVE_TO_LOGIN')), 403);
+			return $this->helper->error(login_box('', $this->language->lang('HAVE_TO_LOGIN')), 403);
 		}
 
 		if (! $this->user_check( $this->user->data['user_id'], $random ) )
 		{
-			return $this->helper->error($this->user->lang('INVALID_LINK_OR_USER'), 403);
+			return $this->helper->error($this->language->lang('INVALID_LINK_OR_USER'), 403);
 		}
 
 		$form_key = 'andreask_ium';
@@ -74,12 +77,12 @@ class main
 		{
 			if ( !check_form_key($form_key) )
 			{
-				$this->helper->error($this->user->lang('FORM_INVALID') . $this->usr_back_link( $this->u_action ), 403);
+				$this->helper->error($this->language->lang('FORM_INVALID') . $this->usr_back_link( $this->u_action ), 403);
 			}
 			// Make sure confirm is selected
 			if ( $this->request->variable('self_delete_verify', '') != 1)
 			{
-				return $this->helper->message($this->user->lang('HAVE_TO_VERIFY') . $this->usr_back_link( $this->u_action ));
+				return $this->helper->message($this->language->lang('HAVE_TO_VERIFY') . $this->usr_back_link( $this->u_action ));
 			}
 
 			// Request to delete user...
@@ -88,12 +91,12 @@ class main
 
 			// log out user.
 			$board_url = generate_board_url(). '/ucp.php?mode=logout&sid='. $this->user->session_id;
-			$message = ($this->config['andreask_ium_approve_del']) ? $this->user->lang('NEEDS_APPROVAL', htmlspecialchars_decode($this->config['sitename'])) : $this->user->lang('USER_SELF_DELETE_SUCCESS', htmlspecialchars_decode($this->config['sitename']));
+			$message = ($this->config['andreask_ium_approve_del']) ? $this->language->lang('NEEDS_APPROVAL', htmlspecialchars_decode($this->config['sitename'])) : $this->language->lang('USER_SELF_DELETE_SUCCESS', htmlspecialchars_decode($this->config['sitename']));
 
 			// meta_refresh has to run before return because after return nothing is going to run...
 			meta_refresh(5, $board_url );
 
-			return $this->helper->message( $this->user->lang( $message ));
+			return $this->helper->message( $this->language->lang( $message ));
 		}
 
 		$this->template->assign_vars(array(
@@ -101,7 +104,7 @@ class main
 					'U_ACTION'  => $this->u_action
 				));
 
-		return $this->helper->render('ium_user_remove.html', $this->user->lang('USER_SELF_DELETE_TITLE'));
+		return $this->helper->render('ium_user_remove.html', $this->language->lang('USER_SELF_DELETE_TITLE'));
 	}
 
 	private function user_check($user, $random)
@@ -123,6 +126,6 @@ class main
 
 	private function usr_back_link($u_action)
 	{
-		return '<br /><br /><a href="' . $u_action . '">&laquo; ' . $this->user->lang['BACK_TO_PREV'] . '</a>';
+		return '<br /><br /><a href="' . $u_action . '">&laquo; ' . $this->language->lang['BACK_TO_PREV'] . '</a>';
 	}
 }
