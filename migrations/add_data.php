659aad8c87a99426b20fa1b5f34f4448408cc920
andreaskou <akourtidis@gmail.com>
2018-07-16 00:46:22 +0200

    Update phpbb_users not custome table, disable unused functions

--- migrations/add_data.php
+++ migrations/add_data.php
@@ -22,18 +22,6 @@ use phpbb\db\migration\migration;
 class add_data extends migration
 {
 
-	private $schema_name='ium_reminder';
-
-	static public function depends_on()
-	{
-		return array('\phpbb\db\migration\data\v31x\v314');
-	}
-
-	// public function effectively_installed()
-	// {
-	// 	return phpbb_version_compare($this->config['andreask_ium_version'], '0.9.0', '>=');
-	// }
-
 	public function update_data()
 	{
 		return array(
@@ -56,24 +44,23 @@ class add_data extends migration
 			array('config_text.add', array('andreask_ium_ignore_forum',	'[]')),
 			// 0.9.9
 			array('config_text.add', array('andreask_ium_ignored_groups', '[]')),
-
 			// cron config
 			array('config.add', array('send_reminder_last_gc', 0, true)),
 			array('config.add', array('send_reminder_gc', (60 * 60 * 24))),
 			array('config.add', array('reminder_limit', 250)),
-			// Initial table population.
-			// array('custom',
-			// 	array(
-			// 		array(
-			// 			$this, 'first_time_install'))),
 			);
 	}
+
+  static public function depends_on()
+  {
+    return array('\phpbb\db\migration\data\v310\gold');
+  }
 
 	public function update_schema()
   {
     return array(
       'add_columns' => array(
-				$this->table_prefix . PHPBB_USERS => array(
+				$this->table_prefix . 'users' => array(
           'ium_remind_counter' => array('UINT', 0),
           'ium_previous_sent_date' => array('TIMESTAMP', 0),
           'ium_reminder_sent_date' => array('TIMESTAMP', 0),
@@ -84,7 +71,6 @@ class add_data extends migration
 					'ium_request_date'	=> array('TIMESTAMP', 0),
 					'ium_request_type'	=> array('VCHAR:10', ''),
 					'ium_random'	=> array('VCHAR:255', 0),
-
 				),
 			),
     );
@@ -94,7 +80,7 @@ class add_data extends migration
 	{
 		return array(
 			'drop_columns' => array(
-				$this->table_prefix . PHPBB_USERS => array(
+				$this->table_prefix . 'users' => array(
 					'ium_remind_counter',
 					'ium_previous_sent_date',
 					'ium_reminder_sent_date',
@@ -110,46 +96,4 @@ class add_data extends migration
 		);
 	}
 
-	// public function first_time_install()
-	// {
-	// 	global $phpbb_container;
-	//
-	// 	if ( !$this->has_users() )
-	// 	{
-	// 		if ( $this->db_tools->sql_table_exists( $this->table_prefix . $this->schema_name ) )
-	// 		{
-	// 			// Current date
-	//
-	// 			$ignore_groups = $phpbb_container->get('auth');
-	//
-	// 			// Get administrator user_ids
-	// 			$administrators = $ignore_groups->acl_get_list(false, 'a_', false);
-	// 			$admin_ary = (!empty($administrators[0]['a_'])) ? $administrators[0]['a_'] : array();
-	//
-	// 			// Get moderator user_ids
-	// 			$moderators = $ignore_groups->acl_get_list(false, 'm_', false);
-	// 			$mod_ary = (!empty($moderators[0]['m_'])) ? $moderators[0]['m_'] : array();
-	//
-	// 			// Merge them together
-	// 			$admin_mod_array = array_unique(array_merge($admin_ary, $mod_ary));
-	//
-	// 			// Make an array of user_types to ignore
-	// 			$ignore_users_extra = array(USER_FOUNDER, USER_IGNORE);
-	//
-	// 			$sql = 'INSERT INTO ' . $this->table_prefix . $this->schema_name . ' (user_id, username)
-	// 			SELECT user_id, username FROM ' . USERS_TABLE . ' p
-	// 			WHERE '. $this->db->sql_in_set('p.user_type', $ignore_users_extra, true) .'
-	// 			AND '. $this->db->sql_in_set('p.user_id', $admin_mod_array, true) . '
-	// 			AND p.user_id > ' . ANONYMOUS;
-	//
-	// 			$result = $this->sql_query($sql);
-	// 		}
-	// 	}
-	// }
-
-	// private function has_users()
-	// {
-	// 	$result = $this->db->get_row_count($this->table_prefix . $this->schema_name);
-	// 	return (bool) $result;
-	// }
 }
