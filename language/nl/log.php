5f80df0ce55a738c8bd03839479ee44fb7e8891e
andreaskou <akourtidis@gmail.com>
2019-02-25 23:58:19 +0100

    new Dutch lang files

--- language/nl/log.php
+++ language/nl/log.php
@@ -5,7 +5,6 @@
 * IUM (Inactive User Manager).
 *
 * @copyright (c) 2016 by Andreas Kourtidis
-* Nederlandse vertaling @ Solidjeuh <https://www.froddelpower.be>
 * @license   GNU General Public License, version 2 (GPL-2.0)
 *
 * For full copyright and license information, please see
