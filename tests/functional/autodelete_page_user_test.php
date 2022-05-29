7fb7f391cad7b91991b2c0ae7989b915dc8dad19
andreaskou <akourtidis@gmail.com>
2022-05-29 22:50:31 +0200

    please?

--- tests/functional/autodelete_page_user_test.php
+++ tests/functional/autodelete_page_user_test.php
@@ -17,4 +17,12 @@ class autodelete_page_user_test extends \phpbb_functional_test_case
         $crawler = self::request('GET', 'app.php/ium/');
 	    self::assert_response_html(404);
 	}
+
+	public function test_user_self_delete_with_random()
+	{
+		$this->login();
+		$this->add_language('andreask/ium', 'user_self_delete_page');
+		$crawler = self::request('GET', 'app.php/ium/1234567?sid=' .  $this->sid);
+		$this->assertContains($this->lang('INVALID_LINK_OR_USER', $crawler->filter('p')->text()))
+	}
 }
