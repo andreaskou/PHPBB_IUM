fdd17be658e4ef23fb94307ef70ff8690dce9f8a
andreaskou <akourtidis@gmail.com>
2022-07-03 23:05:27 +0200

    still nothing!

--- tests/test_config.php
+++ tests/test_config.php
@@ -1,14 +1,13 @@
 <?php
 // This file should be located as: tests/test_config.php
 // So it will be placed after the RUNNING_TESTS.md file.
-$dbms = 'phpbb\\db\\driver\\mysqli';							# Change this when needed
+$dbms = 'phpbb\db\driver\mysqli';
 $dbhost = 'localhost';
 $dbport = '';
-$dbname = 'phpbb_test';											# Change this
-$dbuser = 'root';										# Change this
-$dbpasswd = 'zxcvbn';										# Change this
+$dbname = 'phpbb_tests';
+$dbuser = 'root';
+$dbpasswd = 'zxcvbn';
 $table_prefix = 'phpbb_';
 $phpbb_adm_relative_path = 'adm/';
 $acm_type = 'phpbb\\cache\\driver\\file';
-
-$phpbb_functional_url = 'http://test.devian/';				# Change this when needed
+$phpbb_functional_url = 'http://test.devian/';
